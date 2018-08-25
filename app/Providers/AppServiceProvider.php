7bc5fd63eb882022abf2ebf6c5f864b2fd8b54db
Frankie <101m06008@stud.sju.edu.tw>
2018-08-25 16:09:15 +0800

    [fix] add https://laravel-news.com/laravel-5-4-key-too-long-error 註解

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         require_once (app_path('/Util/helper.php'));
+        
+        //https://laravel-news.com/laravel-5-4-key-too-long-error
         Schema::defaultStringLength(191);
     }
 
